3e14974d24bf681714b9bd28da09fdbceb026edf
Albert <albert.cymbaluk@gmail.com>
2017-07-04 18:38:20 +0200

    add project

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  	sass: {
+  		options: {
+  			sourceMap: true
+  		},
+  		dist: {
+  			files: {
+  				'css/style.css': 'sass/style.sass'
+  			}
+  		}
+  	},
+    jshint: {
+      all: ['js/*.js']
+    }
+  });
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('default', ['jshint', 'sass']);
+};
